366bf940a636aa881ef46120f56ed003a8f8b9a2
Diego <diego@Mac-mini-de-Diego.local>
2026-02-17 16:39:26 -0300

    Primer commit proyecto Spring Boot DevOps

---
+++ src/main/java/cl/duoc/devops/devops/war/DevopsWar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.duoc.devops.devops.war;
+
+/**
+ *
+ * @author diego
+ */
+public class DevopsWar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
